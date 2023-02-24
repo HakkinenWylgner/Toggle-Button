8127721bea94429429aea50f9172c93821c7c9f0
HakkinenWylgner <hakkinenwylgner@gmail.com>
2023-02-24 02:29:18 -0300

    função para toggleButton

--- app/src/main/java/com/example/switchproject/MainActivity.java
+++ app/src/main/java/com/example/switchproject/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.switchproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private ToggleButton toggleSenha;
+    private Switch switchSenha;
+    private TextView textResultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toggleSenha    = findViewById(R.id.toggleSenha);
+        switchSenha    = findViewById(R.id.switchSenha);
+        textResultado  = findViewById(R.id.textResultado);
+
+    }
+    public void enviar (View view) {
+        if (toggleSenha.isChecked()) {
+            textResultado.setText("Switch ligado");
+        } else {
+            textResultado.setText("Switch desligado");
+        }
     }
 }
